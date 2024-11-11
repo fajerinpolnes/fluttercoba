2af04f6fd4870efe961507b10481c26ca24a98f0
fajerinpolnes <fajerin@polnes.ac.id>
2024-11-11 15:17:16 +0800

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/gmits/snap/flutter/common/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/gmits/dev/live/prokit-flutter-single-full-apps/single_full_apps/1-qi-bus_prokit/qi_bus_prokit"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=2.0.0"
+export "FLUTTER_BUILD_NUMBER=2.0.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
